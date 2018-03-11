99ce40be58105d79d1432255d75316dd0ff92781
Thomas Volk <tvolk131@gmail.com>
2018-03-11 15:47:29 -0500

    Swagger has no default response codes

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("route"))
-                .build()
+                .build().useDefaultResponseMessages(false)
                 .apiInfo(metaData());
     }
 

e3d7b1bb61ac980ac538ca5df99365be8bedb4a5
Thomas Volk <tvolk131@gmail.com>
2018-03-11 14:35:01 -0500

    Cleaned up code

--- src/main/java/route/user/UserController.kt
+++ src/main/java/route/user/UserController.kt
@@ -12,10 +12,10 @@ class UserController {
     @RequestMapping(value = "/user/{id}", method = arrayOf(RequestMethod.GET))
     @ApiOperation(value = "Get a user")
     fun getUser(@PathVariable id: String): ResponseEntity<User> {
-        try {
-            return ResponseEntity.ok(User.get(ObjectId(id)))
+        return try {
+            ResponseEntity.ok(User.get(ObjectId(id)))
         } catch (e: Exception) {
-            return ResponseEntity.badRequest().build()
+            ResponseEntity.badRequest().build()
         }
     }
 
@@ -46,7 +46,7 @@ class UserController {
 
     }
 
-    @RequestMapping(value = "/user", method = arrayOf(RequestMethod.PUT))
+    @RequestMapping(value = "/user", method = [(RequestMethod.PUT)])
     @ApiOperation(value = "Create a user")
     fun createUser(@RequestBody user: JsonUser): ResponseEntity<User> {
         return try {
@@ -57,14 +57,14 @@ class UserController {
 
     }
 
-    @RequestMapping(value = "/user/{id}", method = arrayOf(RequestMethod.PATCH))
+    @RequestMapping(value = "/user/{id}", method = [RequestMethod.PATCH])
     @ApiOperation(value = "Edit a user")
     fun patchUser(@RequestBody patchDoc: List<Document>, @PathVariable id: String): ResponseEntity<*> {
         patchDoc.forEach({ doc ->
             run {
-                val op = doc.get("op") as String
-                val path = doc.get("path") as String
-                val value = doc.get("value")
+                val op = doc["op"] as String
+                val path = doc["path"] as String
+                val value = doc["value"]
 
                 if (op == "replace" && path == "/name") {
                     User.get(ObjectId(id)).setName(value as String)
@@ -75,33 +75,33 @@ class UserController {
     }
 
 
-    @RequestMapping(value = "/user/{userId}/friends/{friendId}", method = arrayOf(RequestMethod.PUT))
+    @RequestMapping(value = "/user/{userId}/friends/{friendId}", method = [RequestMethod.PUT])
     @ApiOperation(value = "Add a friend")
     fun addFriend(@PathVariable userId: String, @PathVariable friendId: String): ResponseEntity<HttpStatus> {
         Friend.addFriend(User.get(ObjectId(userId)), User.get(ObjectId(friendId)))
         return ResponseEntity(HttpStatus.CREATED)
     }
 
-    @RequestMapping(value = "/user/{userId}/friends/{friendId}", method = arrayOf(RequestMethod.DELETE))
+    @RequestMapping(value = "/user/{userId}/friends/{friendId}", method = [RequestMethod.DELETE])
     @ApiOperation(value = "Remove a friend")
     fun removeFriend(@PathVariable userId: String, @PathVariable friendId: String): ResponseEntity<*> {
         Friend.removeFriend(User.get(ObjectId(userId)), User.get(ObjectId(friendId)))
         return ResponseEntity.ok().build<Any>()
     }
 
-    @RequestMapping(value = "/user/{id}/friends", method = arrayOf(RequestMethod.GET))
+    @RequestMapping(value = "/user/{id}/friends", method = [RequestMethod.GET])
     @ApiOperation(value = "Get friends")
     fun getFriends(@PathVariable id: String): ResponseEntity<List<User>> {
         return ResponseEntity.ok(Friend.getFriends(User.get(ObjectId(id))))
     }
 
-    @RequestMapping(value = "/user/{id}/friends/requests/sent", method = arrayOf(RequestMethod.GET))
+    @RequestMapping(value = "/user/{id}/friends/requests/sent", method = [RequestMethod.GET])
     @ApiOperation(value = "Get friend requests sent to other users")
     fun getFriendRequestsSent(@PathVariable id: String): ResponseEntity<List<User>> {
         return ResponseEntity.ok(Friend.getSentRequests(User.get(ObjectId(id))))
     }
 
-    @RequestMapping(value = "/user/{id}/friends/requests/received", method = arrayOf(RequestMethod.GET))
+    @RequestMapping(value = "/user/{id}/friends/requests/received", method = [RequestMethod.GET])
     @ApiOperation(value = "Get friend requests received from other users")
     fun getFriendRequestsReceived(@PathVariable id: String): ResponseEntity<List<User>> {
         return ResponseEntity.ok(Friend.getReceivedRequests(User.get(ObjectId(id))))

ffee16655db2f35093f263dd2c46682b9ff4090b
Thomas Volk <tvolk131@gmail.com>
2018-03-10 22:46:02 -0600

    Cleaned up card controller

--- src/main/java/route/card/CardController.kt
+++ src/main/java/route/card/CardController.kt
@@ -1,52 +1,60 @@
 package route.card
 
+import io.swagger.annotations.Api
 import org.bson.Document
 import org.bson.types.ObjectId
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.*
 import route.user.User
 
 @RestController
 class CardController {
-    @RequestMapping(value = "/cardpack", method = arrayOf(RequestMethod.PUT))
+    @RequestMapping(value = "/cardpack", method = [RequestMethod.PUT])
     fun createCardpack(@RequestBody doc: Document): ResponseEntity<Cardpack> {
-        return ResponseEntity.ok(Cardpack.create(doc["name"] as String, User.get(ObjectId(doc["userId"] as String))))
+        val cardpackName = doc["name"] as String
+        val user = User.get(ObjectId(doc["userId"] as String))
+        val cardpack = Cardpack.create(cardpackName, user)
+        return ResponseEntity.created(null).body(cardpack)
     }
 
-    @RequestMapping(value = "/cardpack/{id}", method = arrayOf(RequestMethod.GET))
+    @RequestMapping(value = "/cardpack/{id}", method = [RequestMethod.GET])
     fun getCardpack(@PathVariable id: String): ResponseEntity<Cardpack> {
         return ResponseEntity.ok(Cardpack.get(ObjectId(id)))
     }
 
-    @RequestMapping(value = "/cardpack/{id}", method = arrayOf(RequestMethod.PATCH))
+    @RequestMapping(value = "/cardpack/{id}", method = [RequestMethod.PATCH])
     fun patchCardpack(@RequestBody patchDoc: List<Document>, @PathVariable id: String): ResponseEntity<*>? {
         return null
     }
 
-    @RequestMapping(value = "/cardpack/{id}", method = arrayOf(RequestMethod.DELETE))
+    @RequestMapping(value = "/cardpack/{id}", method = [RequestMethod.DELETE])
     fun deleteCardpack(@RequestBody doc: Document, @PathVariable id: String): ResponseEntity<*>? {
         return null
     }
 
-    @RequestMapping(value = "/cardpack/{id}", method = arrayOf(RequestMethod.PUT))
+    @RequestMapping(value = "/cardpack/{id}", method = [RequestMethod.PUT])
     fun createCard(@RequestBody userDoc: Document, @PathVariable id: String): ResponseEntity<Card>? {
+        // TODO - Allow single card or array
         return null
     }
 
-    @RequestMapping(value = "/cardpack/{id}/cards", method = arrayOf(RequestMethod.GET))
-    fun getCards(@PathVariable id: String): ResponseEntity<Array<Card>>? {
-        return null
+    @RequestMapping(value = "/card/{id}", method = [RequestMethod.DELETE])
+    fun deleteCard(@PathVariable id: String): ResponseEntity<*> {
+        // TODO - Delete card
+        return ResponseEntity.ok(null)
     }
 
-    @RequestMapping(value = "/cardpack/{cardpackId}/card/{cardId}", method = arrayOf(RequestMethod.PATCH))
+    @RequestMapping(value = "/cardpack/{id}/cards", method = [RequestMethod.GET])
+    fun getCards(@PathVariable id: String): ResponseEntity<List<Card>> {
+        return ResponseEntity.ok(Cardpack.get(ObjectId(id)).getCards())
+    }
+
+    @RequestMapping(value = "/cardpack/{cardpackId}/card/{cardId}", method = [RequestMethod.PATCH])
     fun patchCard(@RequestBody patchDoc: List<Document>, @PathVariable cardpackId: String, @PathVariable cardId: String): ResponseEntity<*>? {
         return null
     }
 
-    @RequestMapping(value = "/cardpack/{cardpackId}/card/{cardId}", method = arrayOf(RequestMethod.DELETE))
+    @RequestMapping(value = "/cardpack/{cardpackId}/card/{cardId}", method = [RequestMethod.DELETE])
     fun deleteCard(@PathVariable cardpackId: String, @PathVariable cardId: String): ResponseEntity<*>? {
         return null
     }

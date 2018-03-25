a2245b1703833d5ab2949cc2343a06fe12684e6d
Thomas Volk <tvolk131@gmail.com>
2018-03-25 17:27:08 -0500

    Created mongo implementation of databasecollection

---
+++ src/main/java/database/mongomodel/MongoDatabaseCollection.kt
@@ -0,0 +1,15 @@
+package database.mongomodel
+
+
+import com.mongodb.client.MongoDatabase
+import database.DatabaseCollection
+import route.card.model.CardCollection
+import route.user.model.FriendCollection
+import route.user.model.UserCollection
+
+class MemoryDatabaseCollection(
+        private val mongoDatabase: MongoDatabase,
+        private val mongoUserCollection: MongoUserCollection = MongoUserCollection(mongoDatabase.getCollection("user")),
+        private val mongoFriendCollection: MongoFriendCollection = MongoFriendCollection(mongoDatabase.getCollection("friend"), mongoUserCollection),
+        private val mongoCardCollection: MongoCardCollection = MongoCardCollection(mongoDatabase.getCollection("cardpack"), mongoDatabase.getCollection("card"), mongoUserCollection)
+) : UserCollection by mongoUserCollection, FriendCollection by mongoFriendCollection, CardCollection by mongoCardCollection, DatabaseCollection

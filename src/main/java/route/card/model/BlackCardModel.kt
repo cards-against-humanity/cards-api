c5ef732bb6f5a23c1febd3e24dc5ccd009a5e896
Thomas Volk <tvolk131@gmail.com>
2018-04-04 23:36:18 -0500

    Fixed error in black card model

--- src/main/java/route/card/model/BlackCardModel.kt
+++ src/main/java/route/card/model/BlackCardModel.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 interface BlackCardModel {
     val id: String @JsonProperty(value = "id") get
     val text: String @JsonProperty(value = "text") get
-    val answerFields: Int? @JsonProperty(value = "answerFields") get
+    val answerFields: Int @JsonProperty(value = "answerFields") get
     val cardpackId: String @JsonProperty(value = "cardpackId") get
     @JsonIgnore fun setText(text: String): BlackCardModel
 }
